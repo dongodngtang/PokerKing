572d97cb943cd086c951644c7586a17d6f0cd105
lorne <2270333671@qq.com>
2019-01-23 10:39:41 +0800

    现金桌页面

--- src/pages/CashTable/index.js
+++ src/pages/CashTable/index.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
-import { View,Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity,Image} from 'react-native';
+import {connect} from 'react-redux';
 import styles from './index.style';
+import {Images} from "../../configs/Theme";
 
 @connect(({CashTable}) => ({
-  ...CashTable,
+    ...CashTable,
 }))
 export default class CashTable extends Component {
-  
 
-  componentDidMount(){
 
-  }
+    componentDidMount() {
 
-  render() {
-    return (
-      <View>
-        <Text>CashTable</Text>
-      </View>
-    )
-  }
+    }
+
+    render() {
+        return (
+            <View style={styles.table_view}>
+                <TouchableOpacity style={[styles.click_btn, {marginTop: 28}]}>
+                    <View style={{flex: 1}}/>
+                    <Text style={styles.macao_txt}>{global.lang.t('macao')}>>></Text>
+                    <View style={{flex: 1}}/>
+                    <Image style={{width: 6, height: 12, marginRight: 17}} source={Images.is_right}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.click_btn, {borderTopWidth: 0}]}>
+                    <View style={{flex: 1}}/>
+                    <Text style={styles.macao_txt}>{global.lang.t('manila')}>>></Text>
+                    <View style={{flex: 1}}/>
+                    <Image style={{width: 6, height: 12, marginRight: 17}} source={Images.is_right}/>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
