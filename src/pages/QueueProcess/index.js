78ac7f559cad47c184e1632d1a1a646e018b9ac5
lorne <2270333671@qq.com>
2019-01-28 15:26:35 +0800

    排队进程页面

--- src/pages/QueueProcess/index.js
+++ src/pages/QueueProcess/index.js
@@ -1,31 +1,53 @@
-import React, { Component } from 'react';
-import { View,Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, Text, FlatList, Image} from 'react-native';
+import {connect} from 'react-redux';
 import {logMsg} from "../../utils/utils";
-import styles from './index.style'
+import styles from './index.style';
+import {Metrics, Images} from "../../configs/Theme";
 
 @connect(({QueueProcess}) => ({
-  ...QueueProcess,
+    ...QueueProcess,
 }))
 export default class QueueProcess extends Component {
 
     constructor(props) {
         super(props);
         props.navigation.setParams({
-            title:`${global.lang.t(this.props.params.type)}${global.lang.t('queue_process')}`
+            title: `${global.lang.t(this.props.params.type)}${global.lang.t('queue_process')}`
         })
     }
-  
 
-  componentDidMount(){
 
-  }
+    componentDidMount() {
 
-  render() {
-    return (
-      <View style={styles.process_view}>
-        <Text>QueueProcess</Text>
-      </View>
-    )
-  }
+    }
+
+    _renderItem = (item, index) => {
+        return (
+            <View style={styles.item_view}>
+                <Text style={styles.item_txt}>50/100NL（3桌）</Text>
+                <View style={{flex: 1}}/>
+                <Text style={styles.item_txt}>{global.lang.t('line_count')}：12</Text>
+                <Image style={{height: 12, width: 6}} source={Images.is_right}/>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.process_view}>
+                <FlatList
+                    data={[1, 2, 3, 4]}
+                    showsHorizontalScrollIndicator={false}
+                    ItemSeparatorComponent={this._separator}
+                    renderItem={this._renderItem}/>
+            </View>
+        )
+    }
+
+    _separator = () => {
+        return (
+            <View style={{height: 1, width: Metrics.screenWidth, backgroundColor: "#3F4042"}}/>
+        )
+    }
 }
--- src/pages/QueueProcess/index.style.js
+++ src/pages/QueueProcess/index.style.js
@@ -1,9 +1,21 @@
+import {StyleSheet} from 'react-native';
 
-import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
-    process_view:{
-        flex:1,
-        backgroundColor:"#3F4042"
+    process_view: {
+        flex: 1,
+        backgroundColor: "#3F4042"
+    },
+    item_view: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 17,
+        paddingRight: 18
+    },
+    item_txt:{
+        color:"#ECECEE",
+        fontSize:14
     }
 })
 
