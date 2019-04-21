5d0b3fe455d93018d5c80e18011b33dddb4d0da7
lorne <2270333671@qq.com>
2019-04-21 12:35:36 +0800

    屏蔽生产环境的日志输出

--- index.js
+++ index.js
@@ -3,5 +3,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+if (!__DEV__) {
+    global.console = {
+        info: () => {
+        },
+        log: () => {
+        },
+        warn: () => {
+        },
+        error: () => {
+        },
+    };
+}
 
 AppRegistry.registerComponent(appName, () => App);
