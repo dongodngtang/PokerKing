3e1717a08b8e8c34c24e62b1d2ab69f366f7ba30
lornem <ailuo8899@gmail.com>
2019-06-26 13:51:11 +0800

    首页热门资讯

---
+++ src/pages/Main/Hot.js
@@ -0,0 +1,81 @@
+import React, {PureComponent} from "react";
+import {View, StyleSheet, Text,Image} from 'react-native'
+
+import NotData from "../comm/NotData";
+import UltimateFlatList from "../../components/ultimate/UltimateFlatList";
+import {Images, px2dp, px2sp} from "../../configs/Theme";
+import ImageLoad from "../../components/ImageLoad";
+import {unix_format} from "../../utils/utils";
+
+/**
+ *作者：lorne
+ *时间：2019/6/26
+ *功能：
+ */
+
+const Hot = ({onFetch}) => (
+    <UltimateFlatList
+        ref={(ref) => this.listView = ref}
+        onFetch={onFetch}
+        keyExtractor={(item, index) => `hot_race${index}`}
+        item={_renderItem}
+        refreshableTitlePull={global.lang.t('pull_refresh')}
+        refreshableTitleRelease={global.lang.t('release_refresh')}
+        dateTitle={global.lang.t('last_refresh')}
+        allLoadedText={global.lang.t('no_more')}
+        waitingSpinnerText={global.lang.t('loading')}
+        emptyView={() => <NotData/>}
+    />
+)
+
+
+const _renderItem = (item,index) => (
+    <View
+        key={`hot${index}`}
+        style={styles.item}>
+        <ImageLoad style={styles.img}
+                   source={{uri: item.image}}/>
+        <View style={styles.content}>
+            <Text style={styles.title}>{item.title}</Text>
+            <View style={{flexDirection:'row',alignItems: 'center',width:'100%',marginTop: 5}}>
+                <Image style={{height:px2dp(32),width:px2dp(32)}}
+                source={Images.hot_gary}/>
+                <Text style={[styles.time,{marginLeft:px2dp(14)}]}>{item.source}</Text>
+                <Text style={[styles.time,{marginLeft:px2dp(28)}]}>{unix_format(item.created_at,"MM DD,YYYY")}</Text>
+                <View style={{flex:1}}/>
+                <Image style={{height:px2dp(46),width:px2dp(46),marginRight:px2dp(36)}}
+                source={Images.collect}/>
+                <Image style={{height:px2dp(32),width:px2dp(40),marginRight:px2dp(20)}}
+                       source={Images.share}/>
+            </View>
+
+        </View>
+
+    </View>
+)
+
+const styles = StyleSheet.create({
+    item: {
+        paddingHorizontal: px2dp(22),
+        marginTop: px2dp(18),
+        alignItems: 'center'
+    },
+    img: {
+        height: px2dp(336),
+        width: px2dp(700)
+    },
+    title: {
+        color: '#FFE9AD',
+        fontSize: px2sp(32)
+    },
+    time: {
+        color: '#998E72',
+        fontSize: px2sp(28)
+    },
+    content: {
+        marginVertical: px2dp(24)
+    },
+
+})
+
+export default Hot
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
-import { View,Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {connect} from 'react-redux';
 import Base from "../Base";
 import NavigationBar from "../comm/NavigationBar";
+import ScrollableTab from 'react-native-scrollable-tab-view'
+import {getInfoList} from "../../services/accountDao";
+import {logMsg} from "../../utils/utils";
+import Hot from "./Hot";
 
 
 @connect(({Main}) => ({
-  ...Main,
+    ...Main,
 }))
 export default class Main extends Component {
-  
 
-  componentDidMount(){
 
-  }
+    componentDidMount() {
 
-  render() {
-    return (
-      <Base>
-        <NavigationBar/>
+    }
 
-      </Base>
-    )
-  }
+    onFetch = (page = 1, startFetch, abortFetch)=>{
+        getInfoList({
+            status: 'hot',
+            page,
+            page_size: 20
+        }, data => {
+            logMsg("InfoList:", data)
+            startFetch(data.infos, 20)
+        }, err => {
+            logMsg("reject:", err)
+            abortFetch()
+        })
+    }
+
+    render() {
+        return (
+            <Base>
+                <NavigationBar/>
+                <Hot onFetch={this.onFetch}/>
+
+            </Base>
+        )
+    }
 }
