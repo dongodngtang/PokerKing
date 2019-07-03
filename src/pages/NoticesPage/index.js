4ae8f852531374f6083590a471dfb5f8c3fe9768
lorne <2270333671@qq.com>
2019-07-03 14:37:24 +0800

    首页日期表达

--- src/pages/NoticesPage/index.js
+++ src/pages/NoticesPage/index.js
@@ -23,51 +23,49 @@ export default class NoticesPage extends Component {
         return (
             <View style={{flex: 1, backgroundColor: "#1A1B1F"}}>
                 <View style={{width: Metrics.screenWidth, backgroundColor: "#998E72", height: 1}}/>
-                <UltimateFlatList
-                    ref={(ref) => this.listView = ref}
-                    onFetch={this.onFetch}
-                    separator={this._separator}
-                    keyExtractor={(item, index) => `notices${index}`}
-                    item={this._renderItem}
-                    refreshableTitlePull={global.lang.t('pull_refresh')}
-                    refreshableTitleRelease={global.lang.t('release_refresh')}
-                    dateTitle={global.lang.t('last_refresh')}
-                    allLoadedText={global.lang.t('no_more')}
-                    waitingSpinnerText={global.lang.t('loading')}
-                    emptyView={() => <NotData/>}
-                />
+                <TouchableOpacity style={styles.item_view} onPress={()=>{
+                    router.toInstantList();
+                }}>
+                    <View style={styles.img_view}>
+                        <Image style={styles.notices1_img} source={Images.notices1}/>
+                    </View>
+                    <View style={styles.mid_view}>
+                        <Text style={styles.instants_news}>{global.lang.t('instants_news')}</Text>
+                        <Text style={styles.contents} numberOfLines={1}>{`2019扑克王杯 台湾站即将开赛`}</Text>
+                    </View>
+                    <View style={{flex:1}}/>
+                    <Text style={styles.time_text}>{`2017/8/23`}</Text>
+                </TouchableOpacity>
+                <View style={{width:Metrics.screenWidth - 17,marginLeft:17,backgroundColor:"#998E72",height:1}}/>
+                <TouchableOpacity style={styles.item_view} onPress={()=>{
+                    router.toRankList();
+                }}>
+                    <View style={styles.img_view}>
+                        <Image style={styles.notices1_img} source={Images.notices2}/>
+                    </View>
+                    <View style={styles.mid_view}>
+                        <Text style={styles.instants_news}>{global.lang.t('rank_status')}</Text>
+                        <Text style={styles.contents} numberOfLines={1}>{`2019扑克王杯 台湾站即将开赛`}</Text>
+                    </View>
+                    <View style={{flex:1}}/>
+                    <Text style={styles.time_text}>{`2017/8/23`}</Text>
+                </TouchableOpacity>
+                <View style={{width:Metrics.screenWidth - 17,marginLeft:17,backgroundColor:"#998E72",height:1}}/>
+                {/*<UltimateFlatList*/}
+                    {/*ref={(ref) => this.listView = ref}*/}
+                    {/*onFetch={this.onFetch}*/}
+                    {/*separator={this._separator}*/}
+                    {/*keyExtractor={(item, index) => `notices${index}`}*/}
+                    {/*item={this._renderItem}*/}
+                    {/*refreshableTitlePull={global.lang.t('pull_refresh')}*/}
+                    {/*refreshableTitleRelease={global.lang.t('release_refresh')}*/}
+                    {/*dateTitle={global.lang.t('last_refresh')}*/}
+                    {/*allLoadedText={global.lang.t('no_more')}*/}
+                    {/*waitingSpinnerText={global.lang.t('loading')}*/}
+                    {/*emptyView={() => <NotData/>}*/}
+                {/*/>*/}
             </View>
         )
     }
 
-    onFetch = (page = 1, startFetch, abortFetch) => {
-        startFetch([1, 2, 3, 4, 5, 6], 20)
-    }
-
-    _separator = () => {
-        return <View style={{width:Metrics.screenWidth - 17,marginLeft:17,backgroundColor:"#998E72",height:1}}/>
-    }
-
-    _renderItem=(item,index)=>{
-        const {type} = item;
-        return(
-            <TouchableOpacity style={styles.item_view} onPress={()=>{
-                if(type === 'instants'){
-                    router.toInstantList();
-                }else{
-                    router.toRankList();
-                }
-            }}>
-                <View style={styles.img_view}>
-                    <Image style={styles.notices1_img} source={Images.notices1}/>
-                </View>
-                <View style={styles.mid_view}>
-                    <Text style={styles.instants_news}>{global.lang.t('instants_news')}</Text>
-                    <Text style={styles.contents} numberOfLines={1}>{`2019扑克王杯 台湾站即将开赛`}</Text>
-                </View>
-                <View style={{flex:1}}/>
-                <Text style={styles.time_text}>{`2017/8/23`}</Text>
-            </TouchableOpacity>
-        )
-    }
 }
