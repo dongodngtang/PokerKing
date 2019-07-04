ab94251bbaec71473471d16dc872482dfe0b603c
lorne <2270333671@qq.com>
2019-07-04 11:55:47 +0800

    设置页面

--- src/pages/Setting/index.js
+++ src/pages/Setting/index.js
@@ -1,23 +1,92 @@
-import React, { Component } from 'react';
-import { View,Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
+import styles from './index.style'
+import {Images, Metrics} from "../../configs/Theme";
+import {alertOrder, strNotNull} from "../../utils/utils";
+import {storageLoginUser} from "../../services/accountDao";
 
 
 @connect(({Setting}) => ({
-  ...Setting,
+    ...Setting,
 }))
 export default class Setting extends Component {
-  
 
-  componentDidMount(){
+    state = {
+        race_notice: true,
+        rank_notice: true
+    }
 
-  }
 
-  render() {
-    return (
-      <View style={{flex:1,backgroundColor:"#1A1B1F"}}>
+    componentDidMount() {
 
-      </View>
-    )
-  }
+    }
+
+    _line = () => {
+        return (
+            <View style={{width: Metrics.screenWidth,backgroundColor:"#303236",height:1}}>
+                <View style={{width: Metrics.screenWidth - 19, height: 1, backgroundColor: '#998E72', marginLeft: 19}}/>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: "#1A1B1F"}}>
+                {this._item(global.lang.t('account_security'), Images.right, styles.right_img, null, null)}
+                {this._line()}
+                {this._item(`${global.lang.t('home_language')}(${global.localLanguage})`, Images.right, styles.right_img, null, null)}
+                <View style={{height: 30, width: '100%'}}/>
+                {this._item(global.lang.t('share_friends'), Images.right, styles.right_img, null, null)}
+                {this._line()}
+                {this._item(global.lang.t('feedback'), Images.right, styles.right_img, null, null)}
+                {this._line()}
+                {this._item(global.lang.t('race_notice'), null, null, <View
+                    style={[styles.right_btn, {
+                        backgroundColor: this.state.race_notice ? 'green' : "white",
+                        flexDirection: this.state.race_notice ? 'row-reverse' : "row"
+                    }]}>
+                    <TouchableOpacity activeOpacity={1} style={styles.btn_change} onPress={() => {
+                        this.setState({
+                            race_notice: !this.state.race_notice
+                        })
+                    }}/></View>, null)}
+                {this._line()}
+                {this._item(global.lang.t('rank_notice'), null, null, <View
+                    style={[styles.right_btn, {
+                        backgroundColor: this.state.rank_notice ? 'green' : "white",
+                        flexDirection: this.state.rank_notice ? 'row-reverse' : "row"
+                    }]}>
+                    <TouchableOpacity activeOpacity={1} style={styles.btn_change} onPress={() => {
+                        this.setState({
+                            rank_notice: !this.state.rank_notice
+                        })
+                    }}/></View>, null)}
+
+                {this._line()}
+                {this._item(global.lang.t('clear'), Images.right, styles.right_img, null, '99.2M')}
+                <View style={{height: 14, width: '100%'}}/>
+                {this._item(global.lang.t('version'), Images.right, styles.right_img, null, '2.0')}
+
+                <TouchableOpacity style={styles.drop_out} activeOpacity={1} onPress={()=>{
+                    alertOrder(global.lang.t('is_drop_out'), () => {
+                        storageLoginUser({})
+                    });
+                }}>
+                    <Text style={styles.drop_out_txt}>{global.lang.t('drop_out')}</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    _item = (leftTxt, rightImg, imgStyles, rightView, rightTitle) => {
+        return (
+            <TouchableOpacity  activeOpacity={1} style={styles.btn_view}>
+                <Text style={styles.leftTxt}>{leftTxt}</Text>
+                <View style={{flex: 1}}/>
+                {strNotNull(rightTitle) ? <Text style={styles.rightTitle}>{rightTitle}</Text> : null}
+                {strNotNull(rightImg) ? <Image style={imgStyles} source={rightImg}/> : rightView}
+            </TouchableOpacity>
+        )
+    }
 }
--- src/pages/Setting/index.style.js
+++ src/pages/Setting/index.style.js
@@ -1,6 +1,57 @@
+import {StyleSheet} from 'react-native';
+import {Metrics, px2dp} from "../../configs/Theme";
 
-import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
-
+    btn_view: {
+        width: Metrics.screenWidth,
+        backgroundColor: "#303236",
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50
+    },
+    leftTxt: {
+        color: "#FFE9AD",
+        fontSize: 15,
+        marginLeft: 19
+    },
+    drop_out_txt: {
+        color: "#FFE9AD",
+        fontSize: 17
+    },
+    right_img: {
+        marginRight: 17,
+        width: px2dp(16),
+        height: px2dp(30)
+    },
+    right_btn: {
+        width: 53,
+        height: 34,
+        borderRadius: 17,
+        alignItems: 'center',
+        marginRight: 17,
+        marginLeft:17
+    },
+    btn_change: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        borderWidth:1,
+        borderColor:"#888888",
+        backgroundColor: 'white'
+    },
+    rightTitle:{
+        color: "#998E72",
+        fontSize: 14,
+        marginRight:9
+    },
+    drop_out:{
+        width:Metrics.screenWidth,
+        height:59,
+        backgroundColor:"#303236",
+        alignItems:'center',
+        justifyContent:'center',
+        position:'absolute',
+        bottom:0
+    }
 })
 
