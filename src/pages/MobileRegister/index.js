2b4987eddf06557628421a6c30b8ce0feeb0713f
lorne <2270333671@qq.com>
2019-07-09 18:21:31 +0800

    绑定手机号码页面

--- src/pages/MobileRegister/index.js
+++ src/pages/MobileRegister/index.js
@@ -12,7 +12,7 @@ import CountryPicker, {getAllCountries} from 'react-native-country-picker-modal'
 import DeviceInfo from 'react-native-device-info'
 
 @connect(({MobileRegister}) => ({
-  ...MobileRegister,
+    ...MobileRegister,
 }))
 export default class MobileRegister extends Component {
     constructor(props) {
@@ -42,15 +42,188 @@ export default class MobileRegister extends Component {
         this.vcode = ''
     };
 
-  componentDidMount(){
+    componentDidMount() {
 
-  }
+    }
 
-  render() {
-    return (
-      <View style={{flex:1}}>
-        <Text>MobileRegister</Text>
-      </View>
-    )
-  }
+    render() {
+        const {ext, areaName} = this.state;
+        return (
+            <View style={styles.register_container}>
+
+
+                <TouchableOpacity style={styles.areaView} onPress={() => {
+                    this.areaAction && this.areaAction.openModal();
+                }}>
+                    <CountryPicker
+                        styles={{
+                            touchFlag: {
+                                marginBottom: 12
+                            }
+                        }}
+                        ref={ref => this.areaAction = ref}
+                        filterable
+                        closeable
+                        onChange={value => {
+                            logMsg(value)
+                            this.setState({
+                                areaName: value.name,
+                                ext: value.callingCode,
+                                cca2: value.cca2
+                            })
+                        }}
+                        cca2={this.state.cca2}
+                        translation="eng"
+                    />
+
+                    <Text
+                        style={{width: 180, marginLeft: 8, height: 28, fontSize: 16, color: '#666666'}}>
+                        {`${areaName} (+${this.state.ext})`}
+                    </Text>
+                    <View style={{flex: 1}}/>
+                    <Image style={{width: 6, height: 16, marginRight: 10}} source={Images.is_right}/>
+
+                </TouchableOpacity>
+                <View style={{
+                    height: 1,
+                    marginLeft: 17,
+                    marginRight: 17,
+                    width: Metrics.screenWidth - 34,
+                    backgroundColor: "#ECECEE"
+                }}/>
+                <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={20}>
+                    <View style={styles.textView}>
+                        <TextInput
+                            returnKeyType={'done'}
+                            keyboardType={'numeric'}
+                            style={{
+                                paddingTop: 0,
+                                paddingBottom: 0,
+                                paddingLeft: 0,
+                                marginLeft: 8,
+                                width: '85%',
+                                height: 50,
+                                fontSize: 16,
+                                color: '#444444'
+                            }}
+                            numberOfLines={1}
+                            placeholderTextColor={'#CCCCCC'}
+                            placeholder={global.lang.t('cellphone')}
+                            clearTextOnFocus={true}
+                            underlineColorAndroid={'transparent'}
+                            onChangeText={txt => {
+                                this.iphone = txt
+                            }}
+
+                        />
+
+                    </View>
+                    <View style={{
+                        height: 1,
+                        marginLeft: 17,
+                        marginRight: 17,
+                        width: Metrics.screenWidth - 34,
+                        backgroundColor: "#ECECEE"
+                    }}/>
+                    <View style={styles.textView}>
+                        <TextInput
+                            returnKeyType={'done'}
+                            keyboardType={'numeric'}
+                            style={{
+                                paddingTop: 0,
+                                paddingBottom: 0,
+                                paddingLeft: 0,
+                                marginLeft: 8,
+                                width: 160,
+                                height: 35,
+                                fontSize: 16,
+                                color: '#444444'
+                            }}
+                            maxLength={6}
+                            numberOfLines={1}
+                            placeholderTextColor={'#CCCCCC'}
+                            placeholder={global.lang.t('vscode')}
+                            clearTextOnFocus={true}
+                            underlineColorAndroid={'transparent'}
+                            onChangeText={txt => {
+                                this.vcode = txt
+                            }}
+
+                        />
+                        <View style={{flex: 1}}/>
+                        <View style={{height: 22, width: 1, backgroundColor: '#ECECEE', marginRight: 16}}/>
+                        <CountDownButton
+                            disableBg={'#F3F3F3'}
+                            disableColor={'#747474'}
+                            style={{
+                                marginRight: 8,
+                                height: 50,
+                                backgroundColor: 'white'
+                            }}
+                            textStyle={styles.down_txt}
+                            enable
+                            onClick={counting => {
+                                let iphone = this.iphone
+                                if (isStrNull(iphone)) {
+                                    showToast(global.lang.t('please_input_phone'))
+                                    return
+                                }
+                                postCode({
+                                    mobile: iphone,
+                                    country_code: ext,
+                                    option_type: 'login',
+                                    vcode_type: "mobile",
+                                }, data => {
+                                    counting(true)
+                                }, err => {
+                                    showToast(err)
+                                })
+
+                            }}/>
+
+                    </View>
+                </KeyboardAvoidingView>
+                <View style={{
+                    height: 1,
+                    marginLeft: 17,
+                    marginRight: 17,
+                    width: Metrics.screenWidth - 34,
+                    backgroundColor: "#ECECEE"
+                }}/>
+                <TouchableOpacity style={styles.complete} onPress={() => {
+                    // this._next();
+                }}>
+                    <Text style={{color: '#FFE9AD', fontSize: 17}}>{global.lang.t('complete')}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        marginLeft: 17,
+                        marginRight: 17,
+                        position: 'absolute',
+                        bottom:56, alignSelf:'center', flexDirection: 'row', alignItems: 'center'}}
+                    onPress={() => {
+                        router.toProtocolPage()
+                    }}>
+                    <Text style={{color: "#999999", fontSize: 14}}>{global.lang.t('mobile_prompt')}</Text>
+                </TouchableOpacity>
+
+
+            </View>
+        )
+    };
+
+    onSelectMenu = (index, subindex, data) => {
+        this.setState({
+            ext: data.subtitle,
+            areaName: data.title
+        });
+    };
+
+    changed_ext = (code, name) => {
+        this.setState({
+            ext: code,
+            areaName: name
+        })
+    }
 }
--- src/pages/MobileRegister/index.style.js
+++ src/pages/MobileRegister/index.style.js
@@ -1,6 +1,38 @@
+import {StyleSheet} from 'react-native';
+import {Metrics} from "../../configs/Theme";
 
-import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
-
+    register_container: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        flexDirection:'column',
+        alignItems:'center'
+    },
+    areaView: {
+        width: Metrics.screenWidth - 34,
+        marginTop: 11,
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    textView:{
+        alignSelf:'center',
+        width: Metrics.screenWidth - 34,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    complete:{
+        width: Metrics.screenWidth - 34,
+        marginLeft: 17,
+        marginRight: 17,
+        paddingTop: 12,
+        paddingBottom: 12,
+        backgroundColor: '#212223',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        marginTop: 54
+    }
 })
 
