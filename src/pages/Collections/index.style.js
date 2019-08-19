ac7c738f05a6f9b63b158fffb946f5f364f3cc37
lorne <2270333671@qq.com>
2019-08-19 18:01:44 +0800

    收藏列表的侧滑删除

--- src/pages/Collections/index.style.js
+++ src/pages/Collections/index.style.js
@@ -1,6 +1,21 @@
 
 import { StyleSheet } from 'react-native';
+import {px2dp} from "../../configs/Theme";
 export default StyleSheet.create({
-
+    rowBack:{
+        flex:1,
+        backgroundColor:'#888888',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    delete_img:{
+        width:px2dp(44),
+        height:px2dp(44)
+    },
+    delete_text:{
+        color:"#FFFFFF",
+        fontSize:14
+    }
 })
 
