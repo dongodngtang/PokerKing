7ccd51c17c47ea921926c15e1acabe2476187b98
lorne <2270333671@qq.com>
2019-08-23 18:19:21 +0800

    手机号码修改密码

--- src/pages/ModifyPWDToMobile/index.js
+++ src/pages/ModifyPWDToMobile/index.js
@@ -15,7 +15,7 @@ export default class ModifyPWDToMobile extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            new_show: false,
+            new_show: true,
             vsCode: '',
             new_pwd: '',
             code_show: false
@@ -160,7 +160,7 @@ export default class ModifyPWDToMobile extends Component {
         change_password({
             type : 'vcode',
             vcode:this.state.vsCode,
-            new_pwd:md5.hex_md5(this.state.new_pwd)
+            new_pwd:md5.hex_md5(this.state.new_pwd.trim())
         },res=>{
             showToast(global.lang.t('change_pwd_success'))
         })
