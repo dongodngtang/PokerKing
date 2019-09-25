4ef2f43e50a375435fb937c172aaa62395d2fe75
lorne <2270333671@qq.com>
2019-09-25 14:08:46 +0800

    用户协议中英文

--- src/pages/ProtocolPage/index.js
+++ src/pages/ProtocolPage/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import {connect} from 'react-redux';
-import {protocol_zh} from '../../lang/Protocol'
+import {protocol_zh, protocol_en, protocol_zh_en} from '../../lang/Protocol'
 
 @connect(({ProtocolPage}) => ({
     ...ProtocolPage,
@@ -13,11 +13,24 @@ export default class ProtocolPage extends Component {
 
     }
 
+    get_protocol = () => {
+        let lang = global.localLanguage;
+        if (lang === 'zh') {
+            return protocol_zh
+        } else if (lang === 'en') {
+            return protocol_en
+        } else {
+            return protocol_zh_en
+        }
+
+    }
+
     render() {
+
         return (
-            <ScrollView style={{flex: 1,backgroundColor:'white'}}>
-                <View style={{marginLeft: 17, marginRight: 17,marginTop:10}}>
-                    <Text style={{color:"#444444",fontSize:14,lineHeight:22}}>{protocol_zh}</Text>
+            <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+                <View style={{marginLeft: 17, marginRight: 17, marginTop: 10}}>
+                    <Text style={{color: "#444444", fontSize: 14, lineHeight: 22}}>{this.get_protocol()}</Text>
                 </View>
                 <View style={{height: 80}}/>
             </ScrollView>
