9aec15aac256d65b760879dde52ce915ed00807e
lorne <2270333671@qq.com>
2019-09-26 13:49:46 +0800

    地图报错 撤回了

--- src/pages/CashTable/index.js
+++ src/pages/CashTable/index.js
@@ -1,64 +1,19 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, Image, ImageBackground, StatusBar, Platform} from 'react-native';
+import {View, Text, TouchableOpacity, Image, ImageBackground, FlatList} from 'react-native';
 import {connect} from 'react-redux';
 import styles from './index.style';
-import {Images, px2dp} from "../../configs/Theme";
+import {Images} from "../../configs/Theme";
 import {getCashGames, getCashQueuesNumber} from "../../services/cashTableDao";
-import {getBg, isEmpty, isEmptyObject, isStrNull, logMsg, strNotNull, turn2MapMark} from "../../utils/utils";
+import {getBg, isEmpty, isEmptyObject, logMsg} from "../../utils/utils";
 import {Metrics} from "../../configs/Theme";
 import UltimateFlatList from '../../components/ultimate/UltimateFlatList';
 import NotData from "../comm/NotData";
 import {initLoginUser} from "../../services/accountDao";
-import PopAction from "../comm/PopAction";
-
-
-const list = [{id: 0, name: global.lang.t('Gaode'), type: 'gaode'}, {id: 1, name: global.lang.t('iphone_map'), type: 'pingguo'}];
 
 @connect(({CashTable}) => ({
     ...CashTable,
 }))
 export default class CashTable extends Component {
-
-    state = {
-        current_item: {}
-    }
-
-
-    topBar = () => {
-        return (
-            <View style={styles.navTop}>
-                <StatusBar barStyle={'light-content'}/>
-                <View
-                    onPress={() => {
-
-                    }}
-                    style={styles.left2}>
-                    {/*<Image*/}
-                    {/*style={{height: px2dp(48), width: px2dp(120)}}*/}
-                    {/*source={Images.puke_icon}*/}
-                    {/*/>*/}
-
-                </View>
-                <View
-                    style={styles.navTitle}>
-                    <Text
-                        style={{fontSize: 17, color: '#FFE9AD'}}
-                        numberOfLines={1}>{global.lang.t('room')}</Text>
-                </View>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.toSetting();
-                    }}
-                    style={styles.right2}>
-                    <Image
-                        style={{height: px2dp(38), width: px2dp(36)}}
-                        source={Images.setting}
-                    />
-
-                </TouchableOpacity>
-            </View>
-        )
-    };
 
     _separator = () => {
         return (
@@ -66,53 +21,32 @@ export default class CashTable extends Component {
         )
     };
 
-    getLang = (item) => {
-        const {image_complex, image_en, image} = item;
-        let img = ''
+    getLang=(item)=>{
+        const {image_complex,image_en, image} = item;
         let lang = global.localLanguage;
-        if (lang === 'en') {
-            img = image_en
-        } else if (lang === 'zh-e') {
-            img = image_complex
-        } else {
-            img = image
+        if(lang === 'en'){
+            return image_en
+        }else if(lang === 'zh-e'){
+            return image_complex
+        }else {
+            return image
         }
-        if (isStrNull(img)) {
-            return Images.empty_bg
-        } else {
-            return {uri: img}
-        }
-    };
+    }
 
     _renderItem = (item, index) => {
         let img = this.getLang(item);
-        const {amap_location, amap_navigation_url, amap_poiid, location, name} = item;
         return (
             <TouchableOpacity key={index} activeOpacity={1} onPress={() => {
-                // router.toQueueProcess(item)
-                this.setState({
-                    current_item: item
-                })
-                if (Platform.OS === 'ios') {
-                    this.popAction && this.popAction.toggle();
-                } else {
-                    if (strNotNull(amap_navigation_url))
-                        turn2MapMark(amap_location, amap_navigation_url, amap_poiid, location, name, '')
-
-                }
+                router.toQueueProcess(item)
             }}>
-                <ImageBackground source={img} style={[styles.jinsha, {
-                    flexDirection: "column-reverse"
+                <ImageBackground source={getBg(img)} style={[styles.jinsha, {
+                    flexDirection: "row-reverse",
+                    alignItems: 'center'
                 }]}>
-                    <TouchableOpacity activeOpacity={1}
-                                      style={[styles.txt_view, {backgroundColor: '#101010', opacity: 0.78}]}>
-                        <Text style={styles.txt1} numberOfLines={1}>{item.name}</Text>
-                        <View style={{flex: 1}}/>
-                        {/*<Image*/}
-                        {/*style={{height: px2dp(48), width: px2dp(40),marginRight:17}}*/}
-                        {/*source={Images.location}*/}
-                        {/*/>*/}
-                    </TouchableOpacity>
+                    {/*<View style={styles.txt_view}>*/}
+                    {/*<Text style={styles.txt1}>{global.lang.t('sands_casino')}</Text>*/}
+                    {/*<Text style={styles.txt2}>{global.lang.t('queuing')}></Text>*/}
+                    {/*</View>*/}
                 </ImageBackground>
             </TouchableOpacity>
         )
@@ -121,7 +55,7 @@ export default class CashTable extends Component {
     render() {
         return (
             <View style={styles.table_view}>
-                {this.topBar()}
+
                 <UltimateFlatList
                     firstLoader={true}
                     ref={(ref) => this.listView = ref}
@@ -137,9 +71,6 @@ export default class CashTable extends Component {
                     emptyView={() => <NotData/>}
                 />
 
-                <PopAction
-                    ref={ref => this.popAction = ref}
-                    btnArray={this.popActions()}/>
             </View>
         )
     }
@@ -162,30 +93,5 @@ export default class CashTable extends Component {
         } catch (err) {
             abortFetch();
         }
-    };
-
-    popActions = () => {
-        const {name, location, amap_poiid, amap_navigation_url, amap_location} = this.state.current_item;
-        let reportList = list;
-        let resultArray = [];
-        reportList.forEach((data, index) => {
-            let item = {
-                name: data.name, txtStyle: {color: '#4A90E2'}, onPress: () => {
-                    if (strNotNull(amap_navigation_url)) {
-                        this.popAction.toggle();
-                        turn2MapMark(amap_location, amap_navigation_url, amap_poiid, location, name, data.type)
-                    }
-
-                }
-            };
-            resultArray.push(item);
-        });
-        resultArray.push({
-            name: global.lang.t('cancel'),
-            txtStyle: {color: "#AAAAAA"},
-            onPress: () => this.popAction.toggle()
-        });
-
-        return resultArray;
     };
 }
