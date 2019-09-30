33b4619eff8d8f942f655a0b255d56e64229b238
lorne <2270333671@qq.com>
2019-09-30 16:37:46 +0800

    证件上传页面

--- src/pages/UploadDocument/index.js
+++ src/pages/UploadDocument/index.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, ScrollView, KeyboardAvoidingView, TextInput} from 'react-native';
 import {connect} from 'react-redux';
 import styles from './index.style'
-import {Images, Metrics} from "../../configs/Theme";
+import {Images, Metrics, px2dp} from "../../configs/Theme";
 import {register} from "../../services/accountDao";
 import {isStrNull, showToast} from "../../utils/utils";
 
@@ -12,29 +12,101 @@ import {isStrNull, showToast} from "../../utils/utils";
 }))
 export default class UploadDocument extends Component {
 
+    state = {
+        truth_name: '',
+        cer_no: ''
+    }
+
 
     componentDidMount() {
 
     }
 
     render() {
+        const {truth_name,cer_no} = this.state;
         return (
             <View style={{flex: 1, backgroundColor: "#ECECEE", flexDirection: 'column'}}>
-                <Text style={styles.upload_text}>{global.lang.t('upload_text')}</Text>
-                <TouchableOpacity style={styles.example_view}>
-                    <Image style={styles.example_img} source={Images.example_upload}/>
-                </TouchableOpacity>
-                <View style={{flexDirection:'row',width:Metrics.screenWidth}}>
-                    <Text style={styles.upload_text2}>{global.lang.t('example')}</Text>
+                <ScrollView style={{flex: 1, backgroundColor: "#ECECEE", flexDirection: 'column'}}>
+                    <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={20}>
+                        <View style={styles.message_view}>
+                            <Text style={styles.message_text}>{global.lang.t('truth_name')}</Text>
+                            <TextInput
+                                style={{
+                                    paddingTop: 0,
+                                    paddingBottom: 0,
+                                    paddingLeft: 0,
+                                    marginLeft: 8,
+                                    width: '67%',
+                                    height: 50,
+                                    fontSize: 16,
+                                    color: '#666666',
+                                }}
+                                maxLength={25}
+                                numberOfLines={1}
+                                placeholderTextColor={'#CCCCCC'}
+                                placeholder={global.lang.t('input_truth_name')}
+                                clearTextOnFocus={true}
+                                underlineColorAndroid={'transparent'}
+                                value={truth_name.trim()}
+                                secureTextEntry={this.state.truth_name}
+                                onChange={txt => {
+                                    this.setState({
+                                        truth_name: txt.nativeEvent.text
+                                    })
 
-                    <Image style={styles.example_img} source={Images.example_upload2}/>
-                </View>
+                                }}
+                            />
+                        </View>
+                        <View style={[styles.message_view, {marginTop: 1}]}>
+                            <Text style={styles.message_text}>{global.lang.t('cer_no')}</Text>
+                            <TextInput
+                                style={{
+                                    paddingTop: 0,
+                                    paddingBottom: 0,
+                                    paddingLeft: 0,
+                                    marginLeft: 8,
+                                    width: '67%',
+                                    height: 50,
+                                    fontSize: 16,
+                                    color: '#666666'
+                                }}
+                                numberOfLines={1}
+                                placeholderTextColor={'#CCCCCC'}
+                                placeholder={global.lang.t('input_cer_no')}
+                                clearTextOnFocus={true}
+                                underlineColorAndroid={'transparent'}
+                                value={cer_no.trim()}
+                                secureTextEntry={this.state.cer_no}
+                                onChange={txt => {
+                                    this.setState({
+                                        cer_no: txt.nativeEvent.text
+                                    })
 
-                <TouchableOpacity style={styles.confirm_btn2} onPress={() => {
+                                }}
+                            />
+                        </View>
+                    </KeyboardAvoidingView>
 
-                }}>
-                    <Text style={{color: '#FFE9AD', fontSize: 17}}>{global.lang.t('upload')}</Text>
-                </TouchableOpacity>
+                    <Text style={styles.upload_text}>{global.lang.t('upload_text')}</Text>
+                    <TouchableOpacity style={styles.example_view}>
+                        <Image style={styles.example_img} source={Images.example_upload}/>
+                    </TouchableOpacity>
+                    <View style={{flexDirection: 'row', width: Metrics.screenWidth,alignSelf:'center'}}>
+                        <View style={{width:px2dp(34)}}/>
+                        <Text style={styles.upload_text2}>{global.lang.t('example')}</Text>
+                        <View style={{width:px2dp(44)}}/>
+                        <Image style={styles.example_img} source={Images.example_upload2}/>
+                    </View>
+
+                    <Text style={styles.upload_text3}>{global.lang.t('upload_prompt')}</Text>
+
+                    <TouchableOpacity style={styles.confirm_btn2} onPress={() => {
+
+                    }}>
+                        <Text style={{color: '#FFE9AD', fontSize: 17}}>{global.lang.t('upload')}</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+
 
             </View>
         )
--- src/pages/UploadDocument/index.style.js
+++ src/pages/UploadDocument/index.style.js
@@ -3,17 +3,15 @@ import {Metrics, px2dp} from "../../configs/Theme";
 
 export default StyleSheet.create({
     upload_text: {
-        marginTop: 96,
-        marginLeft: 34,
-        marginBottom: 34,
+        marginTop: px2dp(48),
+        marginLeft: px2dp(34),
+        marginBottom: px2dp(34),
         color: "#AAAAAA",
         fontSize: px2dp(28)
     },
     upload_text2: {
-        marginLeft: 34,
         color: "#AAAAAA",
-        fontSize: px2dp(28),
-        marginRight:4
+        fontSize: px2dp(28)
     },
     example_view: {
         width: px2dp(412),
@@ -22,7 +20,7 @@ export default StyleSheet.create({
         marginBottom: 50
     },
     example_img: {
-        width: px2dp(412),
+        width: px2dp(414),
         height: px2dp(256),
         alignSelf: 'center'
     },
@@ -33,7 +31,27 @@ export default StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: "#161718",
-        marginTop: px2dp(138)
+        marginTop: px2dp(42)
+    },
+    message_view:{
+        marginTop:px2dp(16),
+        width:Metrics.screenWidth,
+        height:px2dp(96),
+        flexDirection:'row',
+        alignItems:'center',
+        backgroundColor:"#FFFFFF"
+    },
+    message_text:{
+        marginLeft: px2dp(34),
+        color: "#333333",
+        fontWeight:'bold',
+        fontSize: px2dp(30)
+    },
+    upload_text3:{
+        marginLeft: px2dp(34),
+        marginTop:px2dp(42),
+        color: "#AAAAAA",
+        fontSize: px2dp(20)
     }
 })
 
