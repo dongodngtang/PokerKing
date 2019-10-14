cf4769678c433df8ce6913d08b55bad0a5328c00
lornem <ailuo8899@gmail.com>
2019-10-14 13:33:56 +0800

    分享切换环境2

--- src/configs/fetch.js
+++ src/configs/fetch.js
@@ -42,11 +42,16 @@ export function initBaseUrl() {
     storage.load({
         key: 'BaseApiType'
     }).then(ret => {
-       typeUrl = ret
-        if(ret === 'test')
-            client.setBaseURL(api.test)
-        else
-            client.setBaseURL(api.production)
+
+        if(ret === 'test'){
+          typeUrl = 'test'
+          client.setBaseURL(api.test)
+        }
+        else{
+          typeUrl = 'production'
+          client.setBaseURL(api.production)
+        }
+
     }).catch(err => {
 
     })
